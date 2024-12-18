7bc325b1c367fbfbb50bea2d98277eb9570f0d33
PressureDraper <warmarines00@hotmail.com>
2024-12-17 21:01:26 -0600

    add: template for routing

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,32 @@
+import { Box, Stack } from "@mui/material"
 
 export const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
+    return (
+        <Stack sx={{ backgroundColor: '#ffffff' }}>
+            <Box component="section" id="selection">
+
+            </Box>
+            <Box component="section" id="about">
+                
+            </Box>
+            <Box component="section" id="career">
+                
+            </Box>
+            <Box component="section" id="contact">
+                
+            </Box>
+            {/* <section id='carousel'>
+                <Inicio />
+            </section>
+            <section id='program'>
+                <Programa />
+            </section>
+            <section id='location'>
+                <Sedes />
+            </section>
+            <section id='contact'>
+                <Contacto />
+            </section> */}
+        </Stack>
+    )
 }
