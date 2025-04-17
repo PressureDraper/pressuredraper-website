9aa38caf4ae8fa2f8d46797bb6cae7fc18519a14
PressureDraper <warmarines00@hotmail.com>
2025-04-16 21:00:46 -0600

    add: continue adding info to projects section

--- src/helpers/projects/data.ts
+++ src/helpers/projects/data.ts
@@ -9,13 +9,13 @@ export const projectsInfo: { img: string, title: string, date: string, desc: str
         img: '/pressuredraper-website/projects/personalWebsite/cover.png',
         title: 'Personal Website',
         date: 'Jan 2025 — Present',
-        desc: 'Personal project developed as a portfolio to display all my professional path as a software engineer as well as a music producer as a hobby.'
+        desc: 'Personal project created as a portfolio to display all my professional path as a software engineer as well as a music producer as a hobby.'
     },
     {
         img: '/pressuredraper-website/projects/hresourcesSystem/cover.png',
         title: 'Human Resources System',
         date: 'Apr 2024 — Present',
-        desc: "System application developed as a microservice inside the Administrative Clinical Integral System platform as a need of human resources department for displaying employee's attendances, incidences, permissions, vacations and holidays with a sub-module created for every functionality."
+        desc: "System application built as a microservice inside the Administrative Clinical Integral System platform as a need of human resources department for displaying employee's attendances, incidences, permissions, vacations and holidays with a sub-module created for every functionality."
     },
     {
         img: '/pressuredraper-website/projects/medicalCongressv2/cover.png',
@@ -24,15 +24,33 @@ export const projectsInfo: { img: string, title: string, date: string, desc: str
         desc: 'Website developed for hosting the medical congress since 2024, remodeling UI/UX design as well as implementing new features for assistance registration and assistants data display over an admin view. Event carried out every year by the High Specialty Medical Center Dr. Rafael Lucio.'
     },
     {
+        img: '/pressuredraper-website/projects/ticketsApp/cover.png',
+        title: 'Tickets App',
+        date: 'Dec 2023',
+        desc: "Desc..."
+    },
+    {
+        img: '/pressuredraper-website/projects/medicalEvaluation/cover.png',
+        title: 'Resident Medical Evaluation System',
+        date: 'Jan 2024 — Present',
+        desc: "Full microservice developed as a need of the teaching department for an evaluation system for the medical residents that arrives to the hospital every year to perform their residency."
+    },
+    {
         img: '/pressuredraper-website/projects/medicalCongressv1/cover.png',
         title: 'Medical Congress Landing Page v1.0.0',
         date: 'Oct 2023 — Nov 2023',
         desc: "Simple Single-Page Application created for hosting the medical congress in 2023 by the High Specialty Medical Center Dr. Rafael Lucio. Displaying only information about the scheduled talks and a form for attendaces' record."
     },
     {
-        img: '/pressuredraper-website/projects/medicalEvaluation/cover.png',
-        title: 'Resident Medical Evaluation System',
-        date: 'Jan 2024 — Present',
-        desc: "Full microservice "
-    }
+        img: '/pressuredraper-website/projects/heroesApp/cover.png',
+        title: 'Heroes App',
+        date: 'Nov 2023',
+        desc: "Desc..."
+    },
+    {
+        img: '/pressuredraper-website/projects/lotus/cover.png',
+        title: 'Lotus Discord Bot',
+        date: 'Feb 2022',
+        desc: "Desc..."
+    },
 ]
