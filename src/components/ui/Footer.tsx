ecd0ae45c4bc87b8c5c5a5d87ee400c77bd7c731
PressureDraper <warmarines00@hotmail.com>
2025-05-20 16:44:20 -0600

    add: production backend url

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -5,61 +5,76 @@ export const Footer = () => {
     const responsive: boolean = useMediaQuery("(max-width : 1050px)");
 
     return (
-        <Grid container sx={{ backgroundColor: 'primary.light', height: 'auto', display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop: 4, position: 'relative', flexDirection: 'column', zIndex: -1 }}>
-            <Box sx={{ display: 'flex', width: 'fit-content' }}>
-                <Box>
-                    <Box sx={{
-                        width: responsive ? '40px' : '80px',
-                        height: responsive ? '40px' : '80px',
-                        backgroundColor: 'primary.100',
-                        borderRadius: '100%',
-                        position: 'relative',
-                        zIndex: 1,
-                        '&::after': {
-                            content: '""',
-                            width: responsive ? '30px' : '70px', // Tamaño del círculo interno
-                            height: responsive ? '30px' : '70px',
-                            backgroundColor: 'primary.light', // Color del círculo interno
-                            borderRadius: '100%',
-                            position: 'absolute',
-                            top: '50%', // Centrar verticalmente
-                            left: '50%', // Centrar horizontalmente
-                            transform: 'translate(-50%, -50%)', // Ajustar al centro
-                        },
-                    }} />
-                </Box>
-                {
-                    sahibUIPalette.map((item: { color: string }) => (
-                        <Box>
-                            <Box sx={{
+        <>
+            <Grid container sx={{
+                backgroundColor: 'primary.light', height: 'auto', display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop: 4, position: 'relative', flexDirection: 'column', zIndex: -1, ":hover": {
+                    backgroundColor: 'red'
+                }
+            }}>
+                <Box sx={{ display: 'flex', width: 'fit-content' }}>
+                    <Box>
+                        <Box
+                            sx={{
                                 width: responsive ? '40px' : '80px',
                                 height: responsive ? '40px' : '80px',
-                                backgroundColor: 'primary.light',
-                                marginLeft: responsive ? '-15px' : '-25px',
+                                backgroundColor: 'primary.100',
                                 borderRadius: '100%',
                                 position: 'relative',
                                 zIndex: 1,
                                 '&::after': {
                                     content: '""',
-                                    width: responsive ? '30px' : '70px', // Tamaño del círculo interno
+                                    display: 'block', // <- IMPORTANTE
+                                    width: responsive ? '30px' : '70px',
                                     height: responsive ? '30px' : '70px',
-                                    backgroundColor: item.color, // Color del círculo interno
+                                    backgroundColor: 'primary.light',
                                     borderRadius: '100%',
                                     position: 'absolute',
-                                    top: '50%', // Centrar verticalmente
-                                    left: '50%', // Centrar horizontalmente
-                                    transform: 'translate(-50%, -50%)', // Ajustar al centro
+                                    top: '50%',
+                                    left: '50%',
+                                    transform: 'translate(-50%, -50%)',
+                                    transition: 'background-color 0.3s', // <- Opcional pero útil
+                                    zIndex: -2, // <- Asegúrate que esté detrás si lo necesitas así
                                 },
-                            }} />
-                        </Box>
-                    ))
-                }
-            </Box>
-            <Box>
-                <Typography sx={{ color: 'primary.600', textAlign: 'center', padding: '3vh 0', fontWeight: 'bold' }}>
-                    © 2025. All rights reserved.
-                </Typography>
-            </Box>
-        </Grid>
+                                '&:hover::after': {
+                                    backgroundColor: 'red',
+                                },
+                            }}
+                        />
+
+                    </Box>
+                    {
+                        sahibUIPalette.map((item: { color: string }) => (
+                            <Box>
+                                <Box sx={{
+                                    width: responsive ? '40px' : '80px',
+                                    height: responsive ? '40px' : '80px',
+                                    backgroundColor: 'primary.light',
+                                    marginLeft: responsive ? '-15px' : '-25px',
+                                    borderRadius: '100%',
+                                    position: 'relative',
+                                    zIndex: 1,
+                                    '&::after': {
+                                        content: '""',
+                                        width: responsive ? '30px' : '70px', // Tamaño del círculo interno
+                                        height: responsive ? '30px' : '70px',
+                                        backgroundColor: item.color, // Color del círculo interno
+                                        borderRadius: '100%',
+                                        position: 'absolute',
+                                        top: '50%', // Centrar verticalmente
+                                        left: '50%', // Centrar horizontalmente
+                                        transform: 'translate(-50%, -50%)', // Ajustar al centro
+                                    },
+                                }} />
+                            </Box>
+                        ))
+                    }
+                </Box>
+                <Box>
+                    <Typography sx={{ color: 'primary.600', textAlign: 'center', padding: '3vh 0', fontWeight: 'bold' }}>
+                        © 2025. All rights reserved.
+                    </Typography>
+                </Box>
+            </Grid>
+        </>
     )
 }
