2056287af4674daa22311d9e865c85f3794c5767
PressureDraper <warmarines00@hotmail.com>
2026-01-14 19:56:22 -0600

    feat(frontend): adding reborn track

--- src/helpers/tracks/trackList.ts
+++ src/helpers/tracks/trackList.ts
@@ -1,5 +1,10 @@
 const trackList = [
     {
+        url: 'https://pub-4a4d714916ec400eb238be8047e509bf.r2.dev/Reborn.mp3',
+        borderShadow: '5px solid rgba(119, 107, 85, 0.2)',
+        boxShadow: '0 0 15px 5px rgba(119, 107, 85, 0.3)'
+    },
+    {
         url: 'https://pub-4a4d714916ec400eb238be8047e509bf.r2.dev/mask.mp3',
         borderShadow: '5px solid rgba(228, 81, 152, 0.2)',
         boxShadow: '0 0 15px 5px rgba(228, 81, 152, 0.3)'
@@ -12,7 +17,7 @@ const trackList = [
     {
         url: 'https://pub-4a4d714916ec400eb238be8047e509bf.r2.dev/clarity.mp3',
         borderShadow: '5px solid rgba(12, 137, 140, 0.2)',
-        boxShadow: 'rgba(12, 137, 140, 0.3)'
+        boxShadow: '0 0 15px 5px rgba(12, 137, 140, 0.3)'
     },
 ]
 
