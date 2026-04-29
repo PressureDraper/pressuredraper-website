8088e3a270a512a1807bc404933ec4216faf0c57
PressureDraper <warmarines00@hotmail.com>
2026-04-29 11:08:37 -0600

    chore(files): next.config adjustments for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    /* output: 'export', */
-    allowedDevOrigins: ['10.30.13.67', '192.168.0.129'],
-    /* basePath: '/pressuredraper-website-v2',
+  /* output: 'export', */
+  /* allowedDevOrigins: ['10.30.13.67', '192.168.0.129'],
+  distDir: 'docs', */
+  /* basePath: '/pressuredraper-website-v2',
   assetPrefix: '/pressuredraper-website-v2', */
 };
 
